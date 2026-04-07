dc1043f85c12c5c712972ae02907878fe9db97ff
6jp9 <jayprakash1405401@gmail.com>
2026-04-07 07:14:08 +0530

    un used code removed

--- Test-1/Jaya Prakash/script/script.js
+++ Test-1/Jaya Prakash/script/script.js
@@ -1,75 +1,73 @@
-const token = localStorage.getItem("userToken");
+let token = localStorage.getItem("userToken");
 if (token) {
   window.location.href = "../templates/index.html";
 }
-
+clearWarnings();
 $("#loginBtn").click(function () {
   $("#loginBtn").addClass("disabled");
   $("#btnText").addClass("d-none");
   $("#spinner").removeClass("d-none");
-
   let userName = $("#userName");
   let userPass = $("#password");
 
   function formValidations(username, password) {
-    return (
+    if (
       username.val().length >= 4 &&
       username.val().length <= 15 &&
       password.val().length >= 8 &&
       password.val().length <= 16
-    );
+    ) {
+      return true;
+    }
+    return false;
   }
 
-  function warnings(type = "validation") {
-    if (type === "validation") {
+  function warnings(useCase = null) {
+    if (useCase) {
       $("#userNameError").removeClass("d-none");
       $("#passwordError").removeClass("d-none");
     } else {
       $("#invalidCreds").removeClass("d-none");
     }
-
     $("#userName").addClass("is-invalid");
     $("#password").addClass("is-invalid");
   }
-
   if (formValidations(userName, userPass)) {
     $.ajax({
       url: "https://dummyjson.com/user/login",
       method: "POST",
-      contentType: "application/json",
-      data: JSON.stringify({
+      data: {
         username: userName.val(),
         password: userPass.val(),
-      }),
+      },
       success: function (response) {
         localStorage.setItem("userToken", response.accessToken);
-
         Swal.fire({
-          title: "Success!",
-          text: "Login successful",
+          title: "success!",
+          text: "Login successfull",
           icon: "success",
-        }).then(() => {
-          window.location.href = "../templates/index.html";
-        });
+        }).then(()=> {window.location.href = "../templates/index.html";})
       },
-      error: function () {
+      error: function (error) {
         $("#loginBtn").removeClass("disabled");
         $("#btnText").removeClass("d-none");
         $("#spinner").addClass("d-none");
-        warnings("invalid");
+        warnings(1);
+        console.log(error);
       },
     });
   } else {
     $("#loginBtn").removeClass("disabled");
     $("#btnText").removeClass("d-none");
     $("#spinner").addClass("d-none");
-    warnings("validation");
+    warnings();
   }
 });
 
 function clearWarnings() {
-  $("#userNameError, #passwordError, #invalidCreds").addClass("d-none");
-  $("#userName, #password").removeClass("is-invalid");
+  $("#nameError").addClass("d-none");
+  $("#passError").addClass("d-none");
+  $("#invalidCreds").addClass("d-none");
+  $("#userName").removeClass("is-invalid");
+  $("#password").removeClass("is-invalid");
 }
-
-$("#userName, #password").on("input", clearWarnings);
